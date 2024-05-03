b54c8171e59879e45aead4bd13838358df88716c
Joshua Hand <joshuabhand@gmail.com>
2024-05-03 16:48:03 -0500

    Add POJO's

---
+++ src/Tool.java
@@ -0,0 +1,29 @@
+public class Tool {
+    private String toolCode;
+    private ToolType toolType;
+    private String brand;
+
+    public String getToolCode() {
+        return toolCode;
+    }
+
+    public void setToolCode(String toolCode) {
+        this.toolCode = toolCode;
+    }
+
+    public ToolType getToolType() {
+        return toolType;
+    }
+
+    public void setToolType(ToolType toolType) {
+        this.toolType = toolType;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
---
+++ src/ToolType.java
@@ -0,0 +1,38 @@
+public class ToolType {
+    private float dailyCharge;
+    private float weekdayCharge;
+    private float weekendCharge;
+    private float holidayCharge;
+
+    public float getDailyCharge() {
+        return dailyCharge;
+    }
+
+    public void setDailyCharge(float dailyCharge) {
+        this.dailyCharge = dailyCharge;
+    }
+
+    public float getWeekdayCharge() {
+        return weekdayCharge;
+    }
+
+    public void setWeekdayCharge(float weekdayCharge) {
+        this.weekdayCharge = weekdayCharge;
+    }
+
+    public float getWeekendCharge() {
+        return weekendCharge;
+    }
+
+    public void setWeekendCharge(float weekendCharge) {
+        this.weekendCharge = weekendCharge;
+    }
+
+    public float getHolidayCharge() {
+        return holidayCharge;
+    }
+
+    public void setHolidayCharge(float holidayCharge) {
+        this.holidayCharge = holidayCharge;
+    }
+}
