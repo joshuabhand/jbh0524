538b2feb668a4c675df3dfff32a4befcd575c3c4
Joshua Hand <joshuabhand@gmail.com>
2024-05-03 17:52:25 -0500

    Add checkout formatting and some date handling

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,4 +1,6 @@
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Inventory {
@@ -19,5 +21,78 @@ public class Inventory {
 
     public void addNewTool(Tool tool) {
         tools.add(tool);
+    }
+
+    public String checkout(Tool tool, int rentalDayCount, int discountPercent, Calendar checkoutDate) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Tool code: ");
+        stringBuilder.append(tool.getToolCode());
+        stringBuilder.append("\n");
+
+        stringBuilder.append("Tool type: ");
+        stringBuilder.append(tool.getToolType().getToolTypeName());
+        stringBuilder.append("\n");
+
+        stringBuilder.append("Tool brand: ");
+        stringBuilder.append(tool.getBrand());
+        stringBuilder.append("\n");
+
+        stringBuilder.append("Rental days: ");
+        stringBuilder.append(rentalDayCount);
+        stringBuilder.append("\n");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yy");
+        stringBuilder.append("Check out date: ");
+        stringBuilder.append(dateFormat.format(checkoutDate.getTime()));
+        stringBuilder.append("\n");
+
+        Calendar dueDate = (Calendar) checkoutDate.clone();
+        dueDate.add(Calendar.DATE, rentalDayCount);
+        stringBuilder.append("Due date: ");
+        stringBuilder.append(dateFormat.format(dueDate.getTime()));
+        stringBuilder.append("\n");
+
+//        stringBuilder.append("Daily rental charge: ");
+//        stringBuilder.append();
+//        stringBuilder.append("\n");
+
+//        stringBuilder.append("Charge days: ");
+//        stringBuilder.append();
+//        stringBuilder.append("\n");
+
+//        stringBuilder.append("Pre-discount charge: ");
+//        stringBuilder.append();
+//        stringBuilder.append("\n");
+
+//        stringBuilder.append("Discount percent: ");
+//        stringBuilder.append();
+//        stringBuilder.append("\n");
+
+//        stringBuilder.append("Discount amount: ");
+//        stringBuilder.append();
+//        stringBuilder.append("\n");
+
+//        stringBuilder.append("Final charge: ");
+//        stringBuilder.append();
+//        stringBuilder.append("\n");
+
+        return stringBuilder.toString();
+        /*
+● Daily rental charge - Amount per day, specified by the tool type.
+● Charge days - Count of chargeable days, from day after checkout through and including due
+date, excluding “no charge” days as specified by the tool type.
+● Pre-discount charge - Calculated as charge days X daily charge. Resulting total rounded half up
+to cents.
+● Discount percent - Specified at checkout.
+● Discount amount - calculated from discount % and pre-discount charge. Resulting amount
+rounded half up to cents.
+● Final charge - Calculated as pre-discount charge - discount amount.
+
+with formatting as follows:
+● Date mm/dd/yy
+● Currency $9,999.99
+● Percent 99%
+        * */
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start");
         Inventory inventory = Inventory.getInstance();
 
         ToolType toolTypeLadder = new ToolType("Ladder", 1.99f, true, true, false);
         Tool toolLadder = new Tool("LADW", toolTypeLadder, "Werner");
         inventory.addNewTool(toolLadder);
 
-
+        String checkoutLadderRental = inventory.checkout(toolLadder, 5, 20, new GregorianCalendar(2024, Calendar.MAY, 3));
+        System.out.println(checkoutLadderRental);
 
     }
 }
