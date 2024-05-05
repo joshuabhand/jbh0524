e913b68bccd466c48c74e796abc967b789eb4fb0
Joshua Hand <joshuabhand@gmail.com>
2024-05-04 21:13:02 -0500

    Acceptance tests pass

--- test/AcceptanceTest.java
+++ test/AcceptanceTest.java
@@ -19,7 +19,7 @@ public class AcceptanceTest {
         inventory.addHoliday(DateHelper.getLaborDay(2020));
 
         ToolType toolTypeLadder = new ToolType("Ladder", 1.99f, true, true, false);
-        ToolType toolTypeChainsaw = new ToolType("Ladder", 1.49f, true, false, true);
+        ToolType toolTypeChainsaw = new ToolType("Chainsaw", 1.49f, true, false, true);
         ToolType toolTypeJackhammer = new ToolType("Jackhammer", 2.99f, true, false, false);
 
         Tool toolLadder = new Tool("LADW", toolTypeLadder, "Werner");
@@ -53,6 +53,9 @@ public class AcceptanceTest {
         LocalDate checkoutDate = LocalDate.of(2020, 7, 2);
         LocalDate dueDate = checkoutDate.plusDays(rentalDays);
 
+        LocalDate independenceDay = inventory.holidayMap.get(DateHelper.getHolidayKey(DateHelper.getObservedIndependenceDay(2020)));
+        assertNotNull(independenceDay);
+
         //TODO Extract hard coded strings
         ToolType toolType = inventory.toolsMap.get("LADW").getToolType();
         int chargeDays = inventory.getChargeDays(checkoutDate, dueDate, toolType);
@@ -71,14 +74,111 @@ public class AcceptanceTest {
         assertEquals("$3.58", NumberFormat.getCurrencyInstance().format(finalAmount));
     }
 
+    @Test
+    public void test3() {
+        int rentalDays = 5;
+        int discount = 25;
+        LocalDate checkoutDate = LocalDate.of(2015, 7, 2);
+        LocalDate dueDate = checkoutDate.plusDays(rentalDays);
+
+        LocalDate independenceDay = inventory.holidayMap.get(DateHelper.getHolidayKey(DateHelper.getObservedIndependenceDay(2015)));
+        assertNotNull(independenceDay);
+
+        ToolType toolType = inventory.toolsMap.get("CHNS").getToolType();
+        int chargeDays = inventory.getChargeDays(checkoutDate, dueDate, toolType);
+        assertEquals(3, chargeDays);
+
+        float dailyCharge = toolType.getDailyCharge();
+        assertEquals(1.49f, dailyCharge);
+
+        float preDiscountCharge = chargeDays * dailyCharge;
+        assertEquals("$4.47", NumberFormat.getCurrencyInstance().format(preDiscountCharge));
+
+        float discountAmount = ((float) discount / 100) * preDiscountCharge;
+        assertEquals("$1.12", NumberFormat.getCurrencyInstance().format(discountAmount));
+
+        float finalAmount = preDiscountCharge - discountAmount;
+        assertEquals("$3.35", NumberFormat.getCurrencyInstance().format(finalAmount));
+    }
+
+    @Test
+    public void test4() {
+        int rentalDays = 6;
+        int discount = 0;
+        LocalDate checkoutDate = LocalDate.of(2015, 9, 3);
+        LocalDate dueDate = checkoutDate.plusDays(rentalDays);
+
+        LocalDate laborDay = inventory.holidayMap.get(DateHelper.getHolidayKey(DateHelper.getLaborDay(2015)));
+        assertNotNull(laborDay);
+
+        ToolType toolType = inventory.toolsMap.get("JAKD").getToolType();
+        int chargeDays = inventory.getChargeDays(checkoutDate, dueDate, toolType);
+        assertEquals(3, chargeDays);
+
+        float dailyCharge = toolType.getDailyCharge();
+        assertEquals(2.99f, dailyCharge);
+
+        float preDiscountCharge = chargeDays * dailyCharge;
+        assertEquals("$8.97", NumberFormat.getCurrencyInstance().format(preDiscountCharge));
+
+        float discountAmount = ((float) discount / 100) * preDiscountCharge;
+        assertEquals("$0.00", NumberFormat.getCurrencyInstance().format(discountAmount));
+
+        float finalAmount = preDiscountCharge - discountAmount;
+        assertEquals("$8.97", NumberFormat.getCurrencyInstance().format(finalAmount));
+    }
+
+    @Test
+    public void test5() {
+        int rentalDays = 9;
+        int discount = 0;
+        LocalDate checkoutDate = LocalDate.of(2015, 7, 2);
+        LocalDate dueDate = checkoutDate.plusDays(rentalDays);
+
+        LocalDate independenceDay = inventory.holidayMap.get(DateHelper.getHolidayKey(DateHelper.getObservedIndependenceDay(2015)));
+        assertNotNull(independenceDay);
+
+        ToolType toolType = inventory.toolsMap.get("JAKR").getToolType();
+        int chargeDays = inventory.getChargeDays(checkoutDate, dueDate, toolType);
+        assertEquals(5, chargeDays);
+
+        float dailyCharge = toolType.getDailyCharge();
+        assertEquals(2.99f, dailyCharge);
+
+        float preDiscountCharge = chargeDays * dailyCharge;
+        assertEquals("$14.95", NumberFormat.getCurrencyInstance().format(preDiscountCharge));
+
+        float discountAmount = ((float) discount / 100) * preDiscountCharge;
+        assertEquals("$0.00", NumberFormat.getCurrencyInstance().format(discountAmount));
+
+        float finalAmount = preDiscountCharge - discountAmount;
+        assertEquals("$14.95", NumberFormat.getCurrencyInstance().format(finalAmount));
+    }
+
+    @Test
+    public void test6() {
+        int rentalDays = 4;
+        int discount = 50;
+        LocalDate checkoutDate = LocalDate.of(2020, 7, 2);
+        LocalDate dueDate = checkoutDate.plusDays(rentalDays);
+
+        LocalDate independenceDay = inventory.holidayMap.get(DateHelper.getHolidayKey(DateHelper.getObservedIndependenceDay(2020)));
+        assertNotNull(independenceDay);
+
+        ToolType toolType = inventory.toolsMap.get("JAKR").getToolType();
+        int chargeDays = inventory.getChargeDays(checkoutDate, dueDate, toolType);
+        assertEquals(1, chargeDays);
+
+        float dailyCharge = toolType.getDailyCharge();
+        assertEquals(2.99f, dailyCharge);
+
+        float preDiscountCharge = chargeDays * dailyCharge;
+        assertEquals("$2.99", NumberFormat.getCurrencyInstance().format(preDiscountCharge));
+
+        float discountAmount = ((float) discount / 100) * preDiscountCharge;
+        assertEquals("$1.50", NumberFormat.getCurrencyInstance().format(discountAmount));
+
+        float finalAmount = preDiscountCharge - discountAmount;
+        assertEquals("$1.50", NumberFormat.getCurrencyInstance().format(finalAmount));
+    }
 }
-/*
-*        Test 1 Test 2 Test 3 Test 4 Test 5 Test 6
-Tool code JAKR LADW CHNS JAKD JAKR JAKR
-Checkout date 9/3/15 7/2/20 7/2/15 9/3/15 7/2/15 7/2/20
-Rental days 5 3 5 6 9 4
-Discount 101% 10% 25% 0% 0% 50%
-*
-*
-*
-* */
